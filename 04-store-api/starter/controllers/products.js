8389e9537c0bcf887737cba34be0bd73f61ab1c0
Syabib Ash-Shiddiqi <sasfort14@gmail.com>
2023-01-21 23:41:06 +0700

    add functionalities for Store API project

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,23 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find(req.query);
+  res.status(200).json({ message: products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ message: products, nbHits: products.length });
+};
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
+};
